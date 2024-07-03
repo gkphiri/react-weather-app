76d7c217fcc2471b42bfa342e46e28657df5dd86
gkphiri <166555047+gkphiri@users.noreply.github.com>
2024-07-03 23:09:13 +0200

    Updated flies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import WeatherSearch from "./WeatherSearch";
+
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
@@ -8,18 +10,6 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <div className="App">
-      <h1>Weather App</h1>
-      <WeatherSearch />
-    </div>
-    <br />
-    <footer>
-      This project was coded by <a href="mailto:kphiri68">Grace Phiri</a> and is{" "}
-      <a href="https://github.com/gkphiri/react-weather-app.git">
-        open-sourced on GitHub
-      </a>
-      and hosted on
-      <a href="https://serene-faun-31cfe3.netlify.app/">Netlify</a>
-    </footer>
+    <App />
   </StrictMode>
 );
